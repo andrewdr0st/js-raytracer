49760f7497903381c4eba3518522931986453284
Andrew Drost <drostandrew@gmail.com>
2024-09-09 14:20:02 -0500

    Multiple spheres

--- compute.js
+++ compute.js
@@ -22,26 +22,39 @@ async function setupGPUDevice(canvas) {
                 pixelDeltaV: vec3<f32>
             };
 
-            @group(0) @binding(0) var<uniform> sphere: vec4<f32>;
-            @group(0) @binding(1) var<uniform> camera: cameraData;
+            @group(0) @binding(0) var<uniform> camera: cameraData;
             @group(1) @binding(0) var tex: texture_storage_2d<rgba8unorm, write>;
+            @group(2) @binding(0) var<storage, read> spheres: array<vec4<f32>>;
 
             @compute @workgroup_size(1, 1, 1) fn rayColor(@builtin(global_invocation_id) id: vec3<u32>) {
                 let pCenter = camera.topLeftPixel + camera.pixelDeltaU * f32(id.x) + camera.pixelDeltaV * f32(id.y);
                 let ray = pCenter - camera.pos;
+                var col: vec4<f32> = vec4<f32>(0, 0, 0, 1);
+                let sphereCount = arrayLength(&spheres);
+                var closestHit: f32 = 10000.0;
 
-                let center: vec3<f32> = sphere.xyz;
-                let radius: f32 = sphere.w;
+                for (var i: u32 = 0; i < sphereCount; i += 2) {
+                    let sphere = spheres[i];
+                    let center: vec3<f32> = sphere.xyz;
+                    let radius: f32 = sphere.w;
 
-                let oc: vec3<f32> = center - camera.pos;
-                let a: f32 = dot(ray, ray);
-                let b: f32 = -2.0 * dot(ray, oc);
-                let c: f32 = dot(oc, oc) - radius * radius;
-                let discriminant: f32 = b * b - 4 * a * c;
+                    let discriminant = hitSphere(center, radius, camera.pos, ray);
 
-                let col: vec4<f32> = select(vec4<f32>(0, 0, 0, 1), vec4<f32>(1, 1, 1, 1), discriminant >= 0);
+                    col = select(col, spheres[i + 1], discriminant < closestHit);
+                    closestHit = min(closestHit, discriminant);
+                }
 
                 textureStore(tex, id.xy, col);
+            }
+
+            fn hitSphere(center: vec3f, r: f32, orig: vec3f, dir: vec3f) -> f32 {
+                let oc = center - orig;
+                let a = dot(dir, dir);
+                let h = dot(dir, oc);
+                let c = dot(oc, oc) - r * r;
+                let d = h * h - a * c;
+
+                return select((h - sqrt(d)) / a, -1.0, d >= 0);
             }
         `
     });
@@ -65,14 +78,6 @@ async function setupGPUDevice(canvas) {
 }
 
 async function renderGPU(camera) {
-
-    const sphereBuffer = device.createBuffer({
-        label: "sphere uniform buffer",
-        size: 16,
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
-    });
-    device.queue.writeBuffer(sphereBuffer, 0, new Float32Array([0, 0, -1, 0.5]));
-
     const cameraBuffer = device.createBuffer({
         label: "camera uniform buffer",
         size: 64,
@@ -86,8 +91,7 @@ async function renderGPU(camera) {
     const uniformBindGroup = device.createBindGroup({
         layout: pipeline.getBindGroupLayout(0),
         entries: [
-            { binding: 0, resource: { buffer: sphereBuffer } },
-            { binding: 1, resource: { buffer: cameraBuffer } }
+            { binding: 0, resource: { buffer: cameraBuffer } }
         ]
     });
 
@@ -100,12 +104,29 @@ async function renderGPU(camera) {
         ]
     });
 
+    let spheres = new Float32Array([0, 0, -1, 0.5, 1, 1, 1, 1, -2, 0.5, -4, 0.75, 0.25, 0.25, 1, 1]);
+
+    const spheresBuffer = device.createBuffer({
+        label: "spheres buffer",
+        size: spheres.byteLength,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
+    });
+    device.queue.writeBuffer(spheresBuffer, 0, spheres);
+
+    const spheresBindGroup = device.createBindGroup({
+        layout: pipeline.getBindGroupLayout(2),
+        entries: [
+            { binding: 0, resource: {buffer: spheresBuffer } }
+        ]
+    });
+
     const encoder = device.createCommandEncoder({ label: "raytrace encoder" });
     const pass = encoder.beginComputePass({ label: "raytrace pass" });
 
     pass.setPipeline(pipeline);
     pass.setBindGroup(0, uniformBindGroup);
-    pass.setBindGroup(1, textureBindGroup)
+    pass.setBindGroup(1, textureBindGroup);
+    pass.setBindGroup(2, spheresBindGroup);
     pass.dispatchWorkgroups(camera.imgW, camera.imgH);
     pass.end();
 
