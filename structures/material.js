5449e528b885018a041051ab71e04cff4872a6a4
Andrew Drost <drostandrew@gmail.com>
2025-07-29 11:14:25 -0500

    Clean up old scripts

--- structures/material.js
+++ structures/material.js
@@ -1,27 +1,8 @@
-let materialId = 0;
-
-class Material {
-    constructor(r, g, b, e, reflectC=0, fuzz=0, ri=0, tex=-1, texA = 0, density = 0, useBDRF = 0, fresnel = 0) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.e = e;
-        this.reflectC = reflectC;
-        this.fuzz = fuzz;
-        this.ri = ri;
-        this.density = density > 0 ? -1 / density : 0;
-        this.tex = tex;
-        this.texA = texA;
-        this.useBDRF = useBDRF;
-        this.fresnel = fresnel;
-        this.id = materialId++;
-    }
-
-    getValues() {
-        return [this.r, this.g, this.b, this.e, this.reflectC, this.fuzz, this.ri, this.density, 0, 0, 0, this.fresnel];
-    }
-
-    getTex() {
-        return new Int32Array([this.tex, this.texA, this.useBDRF]);
+export class Material {
+    constructor(roughness, metallic, refractiveIndex) {
+        this.roughness = roughness;
+        this.metallic = metallic;
+        this.refractiveIndex = refractiveIndex;
+        this.data = new Float32Array([roughness, metallic, refractiveIndex]);
     }
 }
